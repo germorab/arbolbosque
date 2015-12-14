699a80ef628c5a097a99da7d83cc79d742efbfe8
Alonso <alons45@aol.com>
2015-12-14 18:21:52 -0500

    Implemented search in two resources

--- bosque/v1/views.py
+++ bosque/v1/views.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from rest_framework import viewsets
+from rest_framework import filters
 
 from bosque.models import Categoria, Articulo
 from bosque.v1.serializers import CategoriaSerializer, ArticleSerializer
@@ -9,9 +10,13 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('nombre', )
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
 
     queryset = Articulo.objects.all()
     serializer_class = ArticleSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('titulo', )
