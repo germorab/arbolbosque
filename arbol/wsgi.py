6cbfdfc03fde050d20790bcc4e45425bcf9a1811
Alonso Montenegro <alons45@aol.com>
2016-05-23 17:49:00 -0500

    fixed wsgi

--- arbol/wsgi.py
+++ arbol/wsgi.py
@@ -9,9 +9,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arbol.settings")
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
